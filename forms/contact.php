d8394b5e9c6487fd524f0a8bea4d915d6690befc
ssesayjr <43327507+ssesayjr@users.noreply.github.com>
2023-08-08 17:14:58 -0400

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -25,12 +25,12 @@
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
-  $contact->smtp = array(
-    'host' => 'smtp.gmail.com',
-    'username' => 'comm@sbsdyn.com',
-    'password' => 'DynamicCommunication88!',
-    'port' => '25'
-  );
+  // $contact->smtp = array(
+  //   'host' => 'smtp.gmail.com',
+  //   'username' => 'comm@sbsdyn.com',
+  //   'password' => 'DynamicCommunication88!',
+  //   'port' => '25'
+  // );
 
 
   $contact->add_message( $_POST['name'], 'From');
